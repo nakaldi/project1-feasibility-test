3a1f25f9fd5f5dfb50b0bf2b11a976a1853e0c40
Kim Beomhee <nakaldi@gmail.com>
2020-09-09 23:38:53 +0900

    Specify java version

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Maven Hello World!"); }
+        System.out.println("Maven Hello World!");
+    }
 
 }
