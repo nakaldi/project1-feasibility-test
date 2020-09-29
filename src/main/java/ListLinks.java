317107788096d9514b646f14aa5854be267d3a88
Kim Beomhee <nakaldi@gmail.com>
2020-09-29 14:44:43 +0900

    Add testcode for Naver news parsing

--- src/main/java/ListLinks.java
+++ src/main/java/ListLinks.java
@@ -11,15 +11,29 @@ import java.io.IOException;
  */
 public class ListLinks {
     public static void main(String[] args) throws IOException {
-        Validate.isTrue(args.length == 1, "usage: supply url to fetch");
-        String url = args[0];
+        Validate.isTrue(args.length == 0, "usage: supply url to fetch");
+        String url = "https://search.naver.com/search.naver?where=news&query=%EB%B6%81%ED%95%9C&sm=tab_srt&sort=1&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so%3Add%2Cp%3Aall%2Ca%3Aall&mynews=0&refresh_start=0&related=0";
         print("Fetching %s...", url);
 
         Document doc = Jsoup.connect(url).get();
         Elements links = doc.select("a[href]");
         Elements media = doc.select("[src]");
         Elements imports = doc.select("link[href]");
+        Elements articles = doc.select("ul.type01 li");
 
+        print("\nArticles: (%d)", articles.size());
+        for (Element article : articles) {
+            print(" * a: <%s>  (%s)", article.select("a[href]").attr("href"), trim(article.text(), 35));
+        }
+        print("%s\n------------", articles.text());
+        //print("%s\n------------", links);
+        print("%s\n------------", media);
+        print("%s\n------------", imports);
+        //System.out.println(doc.text());
+        System.out.println("-----------------");
+        //System.out.println(doc.html());
+
+/**
         print("\nMedia: (%d)", media.size());
         for (Element src : media) {
             if (src.normalName().equals("img"))
@@ -38,7 +52,7 @@ public class ListLinks {
         print("\nLinks: (%d)", links.size());
         for (Element link : links) {
             print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
-        }
+        }*/
     }
 
     private static void print(String msg, Object... args) {
