919d76cb77e01211591d6cfcfd9440bf8ddabe49
Kim Beomhee <nakaldi@gmail.com>
2020-09-29 19:24:22 +0900

    Add testcodes for Seoul homepage to POST

--- src/main/java/ListLinks.java
+++ src/main/java/ListLinks.java
@@ -1,3 +1,4 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
@@ -5,6 +6,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Example program to list links from a URL.
@@ -41,6 +44,50 @@ public class ListLinks {
         Document doc5 = Jsoup.connect("https://search.daum.net/search?w=news&sort=recency&q=%EB%B6%81%ED%95%9C&cluster=n&DA=STC&dc=STC&pg=1&r=1&p=1&rc=1&at=more&sd=&ed=&period=")
                 .get();
         ParsDaum(doc5);
+
+        //로그인 할때 쓸것. 서울시 사이트는 이거 안쓰고 그냥 검색 data 하나만 넣어주니까 되네
+//        Connection.Response seoulSearch = Jsoup.connect("https://www.seoul.go.kr/realmnews/in/list.do")
+//                .timeout(10 * 1000)
+//                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+//                .header("Accept-Encoding", "gzip, deflate, br")
+//                .header("Accept-Language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7")
+//                .header("Cache-Control", "no-cache")
+//                .header("Connection", "keep-alive")
+//                .header("Content-Length", "64")
+//                .header("Content-Type", "application/x-www-form-urlencoded")
+//                .header("Host", "www.seoul.go.kr")
+//                .header("Origin", "https://www.seoul.go.kr")
+//                .header("Pragma", "no-cache")
+//                .header("Referer", "https://www.seoul.go.kr/realmnews/in/list.do")
+//                .header(" Sec-Fetch-Dest", "document")
+//                .header("Sec-Fetch-Mode", "navigate")
+//                .header("Sec-Fetch-Site", "same-origin")
+//                .header("Sec-Fetch-User", "?1")
+//                .header("Upgrade-Insecure-Requests", "1")
+//                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+//                .method(Connection.Method.GET)
+//                .execute();
+//
+//        Map<String, String> seoulSearchCookies = seoulSearch.cookies();
+//
+//        Map<String, String> data = new HashMap<>();
+//        data.put("searchWord", "청년");
+
+
+        Connection.Response seoulSearchResponse = Jsoup.connect("https://www.seoul.go.kr/realmnews/in/list.do")
+                .timeout(10*1000)
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+                .data("searchWord", "청년")
+                .method(Connection.Method.POST)
+                .execute();
+
+        Document seoulSearchDoc = seoulSearchResponse.parse();
+        Elements seoulNews = seoulSearchDoc.select("div.news-lst div");
+        print("\nseoulNews: (%d)", seoulNews.size());
+        for (Element article : seoulNews) {
+            print(" * a: <%s>  (%s)", article.select("a[href]").attr("href"), trim(article.text(), 35));
+        }
+
 /**
         print("\nMedia: (%d)", media.size());
         for (Element src : media) {
